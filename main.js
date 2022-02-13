d816778f0d1c6bdfa4229e440f09f35b680874e4
furrysweet-bigmuff <greyhotel2@gmail.com>
2022-02-13 17:57:23 +0200

    Decimals, divide by zero and some logic finished

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-let firstValue = null,
-    secondValue = null,
+let resultVal = null,
+    prevVal = 0,
+    curVal = null,
     operator = null,
     clearFlag = true;
 const display = document.getElementById('display'),
       equalBtn = document.getElementById('equal'),
+      clearBtn = document.getElementById('clear'),
+      decimalBtn = document.getElementById('decimal'),
+      undoBtn = document.getElementById('undo'),
       numericButtons = Array.from(document.getElementsByClassName('numeric')),
       actionButtons = Array.from(document.getElementsByClassName('action'));
 
@@ -24,32 +28,70 @@ function divide(a, b) {
 }
 
 function operate() {
-    let a = parseInt(firstValue);
-    let b = null;
-    if (!secondValue) {
-        b = parseInt(display.textContent);
-    } else {
-        b = parseInt(secondValue);
+    // ничего не делать если нажали "равно" до того как выбрать prevVal
+    if (!prevVal) {
+        return
     }
+    let a = parseFloat(prevVal);
+    let b = null;
+    if (!curVal) {
+        // не выполнять operate если ввели только 1 цифру и знак оператор 
+        if (isNum(display.textContent)) {
+            return
+        }
+        b = parseFloat(display.textContent);
+    } else {
+        b = parseFloat(curVal);
+    }
+    console.log(a)
+    console.log(b)
 
     if (operator == '+') {
-        display.textContent = add(a,b)
+        resultVal = add(a,b)
     } else if (operator == '-') {
-        display.textContent = subtract(a,b)
+        resultVal = subtract(a,b)
     } else if (operator == '*') {
-        display.textContent = multiply(a,b)
+        resultVal = multiply(a,b)
     } else if (operator == '/') {
-        display.textContent = divide(a,b)
+        if (b == 0) {
+            divideByZeroError();
+            return
+        }
+        resultVal = divide(a,b)
     }
+
+    if (resultVal.toString().length >= 11) {
+        resultVal = Number(resultVal).toExponential(2)
+    }
+    if (resultVal.toString().length > 6) {
+        display.classList.add('sixDigit');
+    }
+
+    display.textContent = resultVal;
     
-    firstValue = display.textContent;
-    secondValue = null;
+    prevVal = null;
+    resultVal = null;
+    curVal = null;
     operator = null; 
     clearFlag = true;
 }
 
 function numberAction() {
+    if (display.textContent == 'Error') {
+        actionButtons.forEach(button => button.addEventListener('click', operatorAction));
+    }
+    if (display.textContent.length == 6) {
+        display.classList.add('sixDigit');
+    } else if (display.textContent.length == 11) {
+        return
+    }
+    // Не добавляем 0 если он уже на экране
+    if (this.dataset.value == 0 && display.textContent == 0) {
+        return
+    }
+    // Будем дублировать цифру на экран или конкатенировать к числу на экране?
     if (clearFlag) {
+        display.classList.remove('sixDigit');
         display.textContent = this.dataset.value;
         clearFlag = false;
     } else {
@@ -58,21 +100,78 @@ function numberAction() {
 }
 
 function operatorAction() {
-    if (!firstValue) {
-        firstValue = display.textContent;
+    if (!prevVal) {
+        prevVal = display.textContent;
         clearFlag = true;
         display.textContent = this.dataset.value;
         operator = this.dataset.value;
+        display.classList.remove('sixDigit');
     } else {
-        secondValue = display.textContent;
+        curVal = display.textContent;
         clearFlag = true;
         operate();
-        operator = this.dataset.value; 
-    }
+        if (display.textContent == 'Error') {
+            operator = null; 
+            prevVal = null;
+        } else {
+            operator = this.dataset.value; 
+            prevVal = display.textContent;
+        }
 
+    }
+}
+
+function clear() {
+    display.textContent = 0;
+    resultVal = null;
+    prevVal = null;
+    curVal = null;
+    operator = null; 
+    clearFlag = true;
+    display.classList.remove('sixDigit');
+}
+
+function isNum(val) {
+    const arr = ['+', '-', '*', '/', '.'];
+    for (let i = 0; i < arr.length; i++) {
+        if (val == arr[i]) {
+            return true
+        }
+    }
+}
+
+function divideByZeroError() {
+    display.textContent = 'Error';
+    actionButtons.forEach(button => button.removeEventListener('click', operatorAction));
+    resultVal = null;
+    prevVal = null;
+    curVal = null;
+    operator = null; 
+    clearFlag = true;
+}
+
+function decimal() {
+    if (isNum(display.textContent)) {
+        return
+    }
+    let arr = display.textContent.toString().split('');
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '.') {
+            return
+        }
+    }
+    display.textContent += '.';
+    clearFlag = false;
+}
+
+function undo() {
+    
 }
 
 // when user clicks numeric button activate function
 numericButtons.forEach(button => button.addEventListener('click', numberAction));
 actionButtons.forEach(button => button.addEventListener('click', operatorAction));
 equalBtn.addEventListener('click', operate);
+clearBtn.addEventListener('click', clear);
+decimalBtn.addEventListener('click', decimal);
+undoBtn.addEventListener('click', undo);
